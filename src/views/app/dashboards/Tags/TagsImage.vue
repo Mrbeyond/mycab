192161235862af72f752ea3fab135fc208f3eb39
Taiwo <enochtaiwotimothy@gmail.com>
2021-01-30 11:57:50 +0100

    creating analytics

--- src/views/app/dashboards/Tags/TagsImage.vue
+++ src/views/app/dashboards/Tags/TagsImage.vue
@@ -13,18 +13,32 @@
             <b-row  >
                 <span class="float-right">{{data.tag_no}}</span>
              </b-row>
-             <b-row  >
-                 <span class="float-right">{{data.vehicles}}</span>
-             </b-row>
              <b-row>
              <p>{{data.createdAt}}</p>
              </b-row>
              <b-row>
             <p>{{data.updatedAt}}</p>
              </b-row>
+             <v-row>
+                 <b-btn v-b-toggle="data.updatedAt" title="View Vehicles" badge-variant="dark">
+                 Vehicles <b-badge variant="primary" rounded-conner>{{data.vehicles.length}}</b-badge></b-btn>
+             </v-row>
              <b-row>
             <p>{{data.status}}</p>
              </b-row>
+             <b-row>
+            <b-collapse :id="data.createdAt">
+               <div class="p-4 border mt-4">
+                  <b-row>
+                    <b-colxx xxs="12">
+                        <b-card class="mb-4" title="Vehicles">
+                            <b-table responsive bordered :items="data.vehicles" />
+                        </b-card>
+                        </b-colxx>
+                </b-row>
+                </div>
+            </b-collapse>
+     </b-row>
     </b-card-body>
 </b-card>
 </template>
