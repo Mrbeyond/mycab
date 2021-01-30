f03ba00e04ea4967895082af23232d97146935aa
Mrbeyond <beyondbeyond602@gmail.com>
2021-01-30 12:58:05 +0100

    The action dispatch for agents, tags, cards, admins, terminals is done inside the db store module

--- src/store/modules/db.js
+++ src/store/modules/db.js
@@ -1,10 +1,7 @@
 import Axios from 'axios';
 import { PROXY } from '../../constants/config';
-import { ADMINS, RES_KEY, hToken, TAGS, TERMINALS } from '../../constants/formKey';
-// import { TEST, AUTHMODAL, STUDENT, CHANGE_LOGIN_STATUS, INTENDED_PAGE,
-//   AUTH_CONFIRM_ERROR
+import { ADMINS, AGENTS, CARDS, RES_KEY, hToken, TAGS, TERMINALS } from '../../constants/formKey';
 
-// } from '../Constants/storeConst';
 
 export default {
   state: {
@@ -93,7 +90,103 @@ export default {
         }
       })
     },
-  }
+
+  [CARDS]({commit}){
+
+    Axios.get(`${PROXY}admin/cards`, {headers: hToken()})
+    .then(res=>{
+      if(!res.data.error){
+        let payload;
+        try {
+          payload = res.data.data
+          commit(CARDS, payload);
+          commit(RES_KEY, {status:0, owner: CARDS});
+        } catch (e) {
+          commit(RES_KEY, {status:1, owner: CARDS});
+        }
+      }else{
+        commit(RES_KEY, {status:1, owner: CARDS});
+      }
+    })
+    .catch(err => {
+      if(err.response){
+        commit(RES_KEY, {status:2, owner: CARDS});
+      }
+    })
+  },
+
+  [AGENTS]({commit}){
+
+    Axios.get(`${PROXY}admin/agents`, {headers: hToken()})
+    .then(res=>{
+      if(!res.data.error){
+        let payload;
+        try {
+          payload = res.data.data
+          commit(AGENTS, payload);
+          commit(RES_KEY, {status:0, owner: AGENTS});
+        } catch (e) {
+          commit(RES_KEY, {status:1, owner: AGENTS});
+        }
+      }else{
+        commit(RES_KEY, {status:1, owner: AGENTS});
+      }
+    })
+    .catch(err => {
+      if(err.response){
+        commit(RES_KEY, {status:2, owner: AGENTS});
+      }
+    })
+  },
+
+  [ADMINS]({commit}){
+
+    Axios.get(`${PROXY}admin/admins`, {headers: hToken()})
+    .then(res=>{
+      if(!res.data.error){
+        let payload;
+        try {
+          payload = res.data.data
+          commit(ADMINS, payload);
+          commit(RES_KEY, {status:0, owner: ADMINS});
+        } catch (e) {
+          commit(RES_KEY, {status:1, owner: ADMINS});
+        }
+      }else{
+        commit(RES_KEY, {status:1, owner: ADMINS});
+      }
+    })
+    .catch(err => {
+      if(err.response){
+        commit(RES_KEY, {status:2, owner: ADMINS});
+      }
+    })
+  },
+
+  [TERMINALS]({commit}){
+
+    Axios.get(`${PROXY}admin/terminals`, {headers: hToken()})
+    .then(res=>{
+      if(!res.data.error){
+        let payload;
+        try {
+          payload = res.data.data
+          commit(TERMINALS, payload);
+          commit(RES_KEY, {status:0, owner: TERMINALS});
+        } catch (e) {
+          commit(RES_KEY, {status:1, owner: TERMINALS});
+        }
+      }else{
+        commit(RES_KEY, {status:1, owner: TERMINALS});
+      }
+    })
+    .catch(err => {
+      if(err.response){
+        commit(RES_KEY, {status:2, owner: TERMINALS});
+      }
+    })
+  },
+},
 
 }
 
