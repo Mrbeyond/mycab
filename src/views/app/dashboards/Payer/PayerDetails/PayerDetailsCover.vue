999aeb8d64a486d969dd10cab0218af76e3716d0
Mrbeyond <beyondbeyond602@gmail.com>
2021-02-01 12:52:33 +0100

    Stached Payer

--- src/views/app/dashboards/Payer/PayerDetails/PayerDetailsCover.vue
+++ src/views/app/dashboards/Payer/PayerDetails/PayerDetailsCover.vue
@@ -1,8 +1,16 @@
 <template>
-<div>
-   <PayerDetailsBasic  />
-   <PayerDetailsBusiness />
-   <PayerDetailsVehicles />
+<div v-if="isLoading" style="h-100">
+
+  <div class="align-middle">
+    <div class="d-flex justify-content-center">
+      <b-spinner variant="primary" />
+    </div>
+  </div>
+</div>
+<div v-else>
+   <PayerDetailsBasic  :PAYLOAD="basics" />
+   <PayerDetailsBusiness  :PAYLOAD="business" />
+   <PayerDetailsVehicles  :localData="localVehicles" />
    <b-toast :variant="variant" id="example-toast"
       title="Response" auto-hide-delay="8000"
     >
@@ -10,12 +18,13 @@
     </b-toast>
 </div>
 </template>
-<script>
+<script>// @ts-nocheck
+
 import Axios from 'axios';
 import { PROXY } from '../../../../../constants/config';
 import { hToken } from '../../../../../constants/formKey';
 import PayerDetailsBasic from './PayerDetailsBasic.vue';
-import payerDetailsBusiness from './payerDetailsBusiness'
+import PayerDetailsBusiness from './PayerDetailsBusiness.vue'
 import PayerDetailsVehicles from './PayerDetailsVehicles.vue';
 export default {
   components: {
@@ -38,19 +47,45 @@ export default {
 
   }),
   methods: {
-    getTerminals(id){
-      Axios.get(`${PROXY}admin/payer/details/${id}`, {headers: hToken()})
+    getData(id){//${id}
+      Axios.get(`${PROXY}admin/payer/details/141`, {headers: hToken()})
     .then(res=>{
       if(!res.data.error){
-          this.variant = "success";
-          this.resMessage = res.data.message;
+        if(typeof res.data.data === "object"){
+          if (res.data.data.length === 0){
+            this.$router.push(({ path: 'not-found' }));
+            return;
+          }
+        }
+
+        this.variant = "success";
+        this.resMessage = res.data.message;
+        let resData = {...res.data.data[0]};
+        let copyData = {...res.data.data[0]}
+        console.log(resData);
+        const vehicles = copyData.account_vehicles;
+        delete resData.account_vehicles;
+        let account = copyData.account_business_detail;
+        delete resData.account_business_detail;
+        let is_business = copyData.is_business;
+        delete resData.is_business;
+        this.basics = resData;
+        this.isBusiness = is_business;
+        this.localVehicles = vehicles;
+        this.business = account;
+        console.log(this.basics,
+        this.isBusiness,
+        this.localVehicles,
+        this.business
+        );
+        // console.log(this.selectedPayload);
 
         }else{
           this.variant = "danger";
           this.resMessage = "Something went wrong, please retry"
         }
         this.$bvToast.show("example-toast");
-        this.submitting = false;
+        this.isLoading = false;
     })
     .catch(err => {
         this.variant = "danger";
@@ -66,23 +101,26 @@ export default {
          }
         }
         this.$bvToast.show("example-toast");
-        this.submitting = false;
+        this.isLoading = false;
     })
 
     },
   },
-  created(){
+  beforeMount(){
+    // alert(JSON.stringify(this.$router.toString()));
+    let id = this.$route.params.id;
+    this.getData(id);
 
   },
-  // beforeEnter: (to, from, next) => {
-  //   console.log(to, from);
-  //   // alert(to.params.id)
-  // //   next(vm => {
-  // //   // access to component instance via `vm`
-  // // })
-  // next();
+  beforeEnter(to, from, next){
+    console.log(to, from);
+    // alert(to.params.id)
+    next(vm => {
+  //   // access to component instance via `vm`
+  })
+  next();
 
-  // }
+  }
 
 }
 </script>
